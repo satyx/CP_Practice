f0aa9fbe656a021d6b51c06cca08a45de624a885
satyx <satyamcrj@gmail.com>
2019-10-22 02:56:00 +0530

    Faster I/O

---
+++ fast_IO.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);   //desynchronizes C++ standard streams with that of C
+    cin.tie(NULL);      // Unguarantees the flushing of cout stream before the next cin
+    return 0;
+}
